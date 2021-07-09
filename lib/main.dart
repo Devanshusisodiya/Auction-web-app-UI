9a64753a64ed7794b3ec1ab49ee23ed22b1cf0c2
devanshu <devanshukumar45@gmail.com>
2021-07-09 19:38:39 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var status = prefs.getBool('status');
-  var user = prefs.getString('user');
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'KeiBar',
+    title: 'KeiBai',
     theme: ThemeData(
       primarySwatch: Colors.teal,
     ),
